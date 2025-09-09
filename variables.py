80d01e917fe4d37766ca5b5a29e2c8566335f64b
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-09 09:33:07 -0400

    commit

--- variables.py
+++ variables.py
@@ -6,5 +6,27 @@ print(num + x)
 print(x - num)
 print(num * x)
 print(x / num)
+
 sum = num + x
 print (sum)
+x = 5
+y = 10
+z=0
+z=x+y
+print (z)
+z=z/2
+print(z)
+
+name = "Maddox Wiggins"
+occupation = 'Student'
+
+print(name)
+print (occupation)  
+
+
+
+print (f"My name is {name} and my occupation is {occupation}")
+first = 'Calvert'
+second = 'Hall'
+school = first +  second
+print (school)
