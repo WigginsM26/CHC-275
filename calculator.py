1974057cea43594e1a3a6423565378ca50ab70a3
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-10 14:11:30 -0400

    commit

---
+++ calculator.py
@@ -0,0 +1,20 @@
+num1 = input("What is number 1:")
+num2 = input("What is number 2:")
+num1 = int(num1)
+num2 = int(num2)
+sum = num1 + num2
+print (f"The sum of {num1} and {num2} is {sum}")
+
+num3 = input("What is number 3:")
+num4 = input("What is number 4:")
+num3 = int(num3)
+num4 = int(num4)
+div = num3 / num4
+print (f"The divident of {num3} and {num4} is {div}")
+
+num5 = input("What is number 5:")
+num6 = input("What is number 6:")
+num5 = int(num5)
+num6 = int(num6)
+multi = num5 * num6
+print (f"The product of {num5} and {num6} is {multi}")
--- variables.py
+++ variables.py
@@ -30,3 +30,30 @@ first = 'Calvert'
 second = 'Hall'
 school = first +  second
 print (school)
+
+name = "Maddox Wiggins"
+occupation = 'Student'
+
+print(name)
+print (occupation)  
+
+
+
+print (f"My name is {name} and my occupation is {occupation}")
+first = 'Calvert'
+second = 'Hall'
+school = first + second
+print (school)
+
+print ("Welcome to Calvert Hall")
+
+name = input("What is your name? ")
+gradyear = input ("What year do you graduate? ")
+standing = input ("What is your class standing? ")
+email = input ("What is your email? ")
+print (f"My name is {name}, I graduate is {gradyear}, my class is {standing}, and my email is {email}")
+
+x = input("What is num1: ")
+y = input("What is num2: ")
+z = x + y 
+print(z)
