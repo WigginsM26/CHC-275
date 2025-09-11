3528b6cd7f3679d46239c70794105c113249294e
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-11 09:29:17 -0400

    Create branching.py

---
+++ branching.py
@@ -0,0 +1,23 @@
+x = 10
+if x % 2 == 1:
+    print(f"{x} is divisble by 2")
+    print ("Test")
+
+    print(" This is not part of the if statement")    
+
+x = 10
+if 10 % 2 == 1:
+    print(f"{x} is divisble by 2")
+    print ("Test")
+else:
+    print(" This is not part of the if statement")    
+
+
+x = 67
+if x % 5 == 0:
+    print(f"{x} is divisble by 5")
+elif x % 2 == 0:
+    print(f"{x} is divisible by 2")
+else:
+    print (f"{x} is not divisible by 5 or 2")
+    
