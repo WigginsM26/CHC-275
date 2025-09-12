6ec91a98a9623843a579d0412e8f80fbda9efb2e
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-12 14:15:54 -0400

    commit

---
+++ and-or branching.py
@@ -0,0 +1,17 @@
+x = 10
+if x > 5 and x > 15:
+    print (f"{x} is greater then 5 and 15")
+
+if x > 5 and x > 15:
+    print (f"{x} is greater then 5 or 15")
+    
+    
+year = input("Enter your year: ")
+if year == "freshman":
+    print ("your grade level is 9")
+elif year == "sophmore":
+    print("your grade level is 10")
+elif year == "junior":
+    print ("your grade level is 11") 
+elif year == ("Senoir"):
+    print ("your grade level is 12")
--- branching.py
+++ branching.py
@@ -13,11 +13,17 @@ else:
     print(" This is not part of the if statement")    
 
 
-x = 67
+x = 3
 if x % 5 == 0:
     print(f"{x} is divisble by 5")
 elif x % 2 == 0:
     print(f"{x} is divisible by 2")
 else:
     print (f"{x} is not divisible by 5 or 2")
-    
+
+
+x = 10
+if x > 5 and x > 15:
+    print (f"{x} is greater then 5 and 15")
+if x > 5 and x > 15:
+    print (f"{x} is greater then 5 or 15")
