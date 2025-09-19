7f8b8552e4a30e6d4a2c28b44888c7afcd9d23cf
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-19 09:30:41 -0400

    commit

---
+++ Hailstone.py
@@ -0,0 +1,32 @@
+x = input("Enter your number ")
+x = int(x)
+check = False
+while x != 1:
+    if x % 2 == 1:
+        x = x*3+1
+        print(x)
+    else:
+        x = x/2
+        print(x)
+      
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
