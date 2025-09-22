811199a96de8eb7e86895efe0ad612ab9f9cc9a9
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-22 14:15:18 -0400

    commit

---
+++ fish.py
@@ -0,0 +1,13 @@
+fish = input("What type of fish do you want? ")
+if fish == "carnivorous":
+    fish == input("Do you have the fish already? ")
+    if fish == "yes":
+        print ("TOO BAD")
+    if fish == "no":
+        print ("enjoy")
+      
+elif fish == "salt water":
+    print("you're a fancy fish parent")
+elif fish == "community":
+    print ("you should get more than one")
+else: fish == ("I don't think that's a type of fish")
