a549d06664eabdc095e317b5f6790eabb27cc822
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-29 09:30:05 -0400

    commit

--- myList = [5,10,15,20,25,30,35].py
+++ myList = [5,10,15,20,25,30,35].py
@@ -2,9 +2,7 @@ myList = [5,10,15,20,25,30,35]
 print("for loop example")
 for num in myList:
     print(num)
-   
 print(myList)
-
 i=0
 while i <= len (myList) -1:
     myList[i] = myList [i]+ 5
@@ -12,3 +10,7 @@ while i <= len (myList) -1:
     
 print(myList)
     
+myList = [1,2,3,4,5]
+for i in range (len(myList)):
+    myList[i] = myList [i] + 5
+print(myList)
