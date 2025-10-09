71d27800e0f1d5e74ec07fac820fb983f6e68770
WigginsM26 <WigginsM26@chcstudent.com>
2025-10-09 11:12:58 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,66 +1,53 @@
-name = ["Maddox","Paul","Mr.Basmaci", ]
-accname = ["@wiggins","@paul","@basmaci"]
+acc = ["@wiggins","@paul","@basmaci"]
 balance = [10000,70,5000]
-for i in range (len(name)):
-    print (f"Banking information for {name[i]}. {accname[i]}. {balance[i]}")
 
-
-
-
-
-
-name = input("Welcome to the Bank! Enter your account name: ")
-if name == "@wiggins":
-    password = input("What is you password?: ") 
-    if password == "maddox!":
-        print ("You have signed in!")
-    check = False
-
+check = False
 while check == False:
-    print("1. deposit")
-    print("2. withdraw")
-    print("3. log in ")
-    print("4. sign in ")
-    print("5. transfer")
-    oper = input(" What would you like to do?: ")
-    if oper == "deposit":
-        num = input("What amount?: ")
-        num = int(num)
-    
-        print (f"You have deposited ${num} into your account: ")
+    for i in range (len(acc)):
+         print (f"Banking information for. {acc[i]}. Balance: {balance[i]}") 
+    print("1. Deposit")
+    print("2. Withdrawl")
+    print("3. Add Account")
+    print("4. Delete Account")
+    print("5. Transfer")
 
-    elif oper == "withdraw":
-        num7 = input("What is number 7:")
-        num8 = input("What is number 8:")
-        num7 = int(num7)
-        num8 = int(num8)
-        diff = num7 - num8
-        print (f"The difference of {num7} and {num8} is {diff}")
+    option = input("What would you like to do today! Press quit to exit: ")
 
-    elif oper == "sign in":
-        num5 = input("What is number 5:")
-        num6 = input("What is number 6:")
-        num5 = int(num5)
-        num6 = int(num6)
-        multi = num5 * num6
-        print (f"The product of {num5} and {num6} is {multi}")
-    
-    elif oper == ("log in"):
-        num3 = input("What is number 3:")
-        num4 = input("What is number 4:")
-        num3 = int(num3)
-        num4 = int(num4)
-        div = num3 / num4
-        print (f"The divident of {num3} and {num4} is {div}")
-        
-    elif oper == "transfer":
-        num5 = input("What is number 5:")
-        num6 = input("What is number 6:")
-        num5 = int(num5)
-        num6 = int(num6)
-        multi = num5 * num6
-        print (f"The product of {num5} and {num6} is {multi}")
-    elif oper == "quit":
-        check = True
-        
-     
+    if option == "Deposit":
+        name = input("Which account is depositing? ")   #deposit corresponds to print 1. my account correspoonds to the balances already set. Adds to already set balance.
+        index = acc.index(name)
+        deposit = input("State the amount being deposited: ")   
+        deposit = int(deposit)
+        balance[index] = balance[index] + deposit
+
+    elif option == "Withdrawl":
+        name = input("Which account is withdrawing? ") # same as deposit but it subracts from balance which is my second list. 
+        index = acc.index(name)
+        deposit = input("State the amount being withdrawn: ")  
+        deposit = int(deposit)
+        balance[index] = balance[index] - deposit
+
+    elif option == "Add":
+        name = input("Add account: ")   #T
+        acc.append(name)
+        num = input("How much is on your account? ") 
+        balance.append(num)
+
+    elif option == "Delete":
+        name = input("Remove account: ")  #
+        index = acc.append(name)
+        acc.pop(index)
+        balance.pop(index)
+
+    elif option == "Transfer":
+        name = input("Which account is making the transfer?: ")  #
+        index = acc.index(name)
+        name2 = input("To which account?: ") 
+        index2 = acc.index(name2)
+        deposit = input("State the amount being transferred: ")
+        deposit = int(deposit)
+        balance[index] = balance[index] - deposit
+        balance[index2] = balance[index2] + deposit
+
+    if option == "quit":
+            check = True
