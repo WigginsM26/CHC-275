020e7cc53d0561892365aff02d2809f22b907c1e
WigginsM26 <WigginsM26@chcstudent.com>
2025-10-08 22:47:31 -0400

    commit

--- bank.py
+++ bank.py
@@ -14,107 +14,41 @@ while check == False:
 option = input("What would you like to do today! Press quit to exit: ")
 
 if option == "1":
-        name = input("Which account is depositing? ")
-        index = acc.index(name)
-        deposit = input("State the amount being deposited: ")
-        deposit = int(deposit)
-        balance[index] = balance[index] + deposit
+    name = input("Which account is depositing? ")
+    index = acc.index(name)
+    deposit = input("State the amount being deposited: ")
+    deposit = int(deposit)
+    balance[index] = balance[index] + deposit
 
-"""elif option == "2":
-        name = input("Remove account: ")
-        index = bank1.append(name)
-        acc.pop(index)
-        acc.pop(index)
+elif option == "2":
+    name = input("Which account is withdrawing? ")
+    index = acc.index(name)
+    deposit = input("State the amount being withdrawn: ")
+    deposit = int(deposit)
+    balance[index] = balance[index] - deposit
 
-    elif option == "3":
-        name = input("Which account is depositing? ")
-        index = bank1.index(name)
-        money = input("State the amount being deposited: ")
-        money = int(money)
-        bank2[index] = bank2[index] + money
+elif option == "3":
+    name = input("Add account: ")
+    acc.append(name)
+    num = input("How much is on your account? ")
+    num = int(num)
+    balance.append(num)
 
-    elif option == "4":
-        name = input("Which account is withdrawing? ")
-        index = bank1.index(name)
-        money = input("State the amount being withdrawn: ")
-        money = int(money)
-        bank2[index] = bank2[index] - money
+elif option == "4":
+    name = input("Remove account: ")
+    index = acc.append(name)
+    acc.pop(index)
+    balance.pop(index)
 
-    elif option == "5":
-        name = input("Which account is making the transfer?: ")
-        index = bank1.index(name)
-        name2 = input("To which account?: ")
-        index2 = bank1.index(name2)
-        transfer = input("State the amount being transferred: ")
-        transfer = int(transfer)
-        bank2[index] = bank2[index] - transfer
-        bank2[index2] = bank2[index2] + transfer
+elif option == "5":
+    name = input("Which account is making the transfer?: ")
+    index = acc.index(name)
+    name2 = input("To which account?: ")
+    index2 = acc.index(name2)
+    deposit = input("State the amount being transferred: ")
+    deposit = int(deposit)
+    balance[index] = balance[index] - deposit
+    balance[index2] = balance[index2] + deposit
 
-    if option == "quit":
+if option == "quit":
         check = True
-
-
-
-
-
-
-
-
-
-
-
-name = input("Welcome to the Bank! Enter your account name: ")
-if name == "@wiggins":
-    password = input("What is you password?: ") 
-    if password == "maddox!":
-        print ("You have signed in!")
-    check = False
-
-while check == False:
-    print("1. deposit")
-    print("2. withdraw")
-    print("3. log in ")
-    print("4. sign in ")
-    print("5. transfer")
-    oper = input(" What would you like to do?: ")
-    if oper == "deposit":
-        num = input("What amount?: ")
-        num = int(num)
-    
-        print (f"You have deposited ${num} into your account: ")
-
-    elif oper == "withdraw":
-        num7 = input("What is number 7:")
-        num8 = input("What is number 8:")
-        num7 = int(num7)
-        num8 = int(num8)
-        diff = num7 - num8
-        print (f"The difference of {num7} and {num8} is {diff}")
-
-    elif oper == "sign in":
-        num5 = input("What is number 5:")
-        num6 = input("What is number 6:")
-        num5 = int(num5)
-        num6 = int(num6)
-        multi = num5 * num6
-        print (f"The product of {num5} and {num6} is {multi}")
-    
-    elif oper == ("log in"):
-        num3 = input("What is number 3:")
-        num4 = input("What is number 4:")
-        num3 = int(num3)
-        num4 = int(num4)
-        div = num3 / num4
-        print (f"The divident of {num3} and {num4} is {div}")
-        
-    elif oper == "transfer":
-        num5 = input("What is number 5:")
-        num6 = input("What is number 6:")
-        num5 = int(num5)
-        num6 = int(num6)
-        multi = num5 * num6
-        print (f"The product of {num5} and {num6} is {multi}")
-    elif oper == "quit":
-        check = True"""
-        
-     
