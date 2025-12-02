124b0cb09455f6a4e62166c9fd173f02e13c254a
WigginsM26 <WigginsM26@chcstudent.com>
2025-12-02 12:57:35 -0500

    Update GroceryStore.py

--- GroceryStore.py
+++ GroceryStore.py
@@ -6,47 +6,45 @@ cart = {}
 check = False
 while not check:
     for i in range(len(items)):
-        print(f"{items[i]} - ${prices[i]:.2f}")
+        print(f"{items[i]} - ${prices[i]}")
     
     print("1. Add to cart")
     print("2. Remove from cart")
     print("3. View cart")
     print("4. Checkout")
-    option = input("Choose an option (or type 'quit' to exit): ")
+    option = input("Choose an option or quit: ")
 
     if option == "1":
-        name = input("Enter item to add: ")
+        name = input("Enter item: ")
         if name in items:
-            qty = int(input("Quantity: "))
-            cart[name] = cart.get(name, 0) + qty
-            print(f"Added {qty} {name} to cart.")
+            amt = int(input("Amount: "))
+            cart[name] = cart.get(name, 0) + amt #looked up how to use .get to simplify adding to cart
+            print(f"Added {amt} {name} to cart.")
         else:
             print("Item not found.")
 
     elif option == "2":
         name = input("Enter item to remove: ")
         if name in cart:
-            qty = int(input("Quantity to remove: "))
-            if qty >= cart[name]:
-                del cart[name]
+            amt = int(input("Amount you want to remove: "))
+            if amt >= cart[name]:
+                 cart.pop[name]
             else:
-                cart[name] -= qty
-            print(f"Removed {qty} {name} from cart.")
+                cart[name] -= amt
+            print(f"Removed {amt} {name} from cart.")
         else:
             print("Item not in cart.")
 
     elif option == "3":
         print("Your cart:")
-        for item, qty in cart.items():
-            print(f"{qty} x {item}")
+        for item, amt in cart.items():
+            print(f"{amt} x {item}")
 
     elif option == "4":
-        subtotal = sum(prices[items.index(item)] * qty for item, qty in cart.items())
+        subtotal = sum(prices[items.index(item)] * amt for item, amt in cart.items())
         tax = subtotal * 0.06
         total = subtotal + tax
-        print(f"Subtotal: ${subtotal:.2f}")
-        print(f"Tax: ${tax:.2f}")
-        print(f"Total: ${total:.2f}")
+        print(f"Total: ${total}") #didn't know if you wanted the subtotal and tax shown
         check = True
 
     elif option == "quit":
